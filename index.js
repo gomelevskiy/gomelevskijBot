93d097a7fc174614dae889eb11af17ce71aec1f0
админ <admin@iMac.local>
2019-04-24 13:37:23 +0300

    edit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Telegraf = require('telegraf');
-const app = new Telegraf(process.env.BOT_TOKEN);
+const app = new Telegraf('899741222:AAFV4NVV9B13QgG6quUuk0T1HI-jA1LYQh0');
 
 app.use((ctx, next) => {
   const start = new Date();
