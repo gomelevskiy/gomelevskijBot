e7adee6e2812af1f14cd04f7233ae49e86d42fc7
админ <admin@iMac.local>
2019-04-24 13:27:39 +0300

    ed

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-console.log("basic");
 const Telegraf = require('telegraf');
 const app = new Telegraf(process.env.BOT_TOKEN);
 
@@ -13,3 +12,8 @@ app.use((ctx, next) => {
 app.on('start', (ctx) => ctx.reply('Welcome, my dear friend!'));
 app.on('test', (ctx) => ctx.reply('Test me, please!'));
 app.launch();
+
+let port = process.env.PORT || 8000;
+server.listen(port, function() {
+    console.log("App is running on port " + port);
+});
