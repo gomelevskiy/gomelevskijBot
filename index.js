052312df6c81c94184ef44bf58448a35c071a8b9
админ <admin@iMac.local>
2019-04-24 19:32:52 +0300

    test

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = new Telegraf(process.env.BOT_TOKEN);
 app.start((ctx) => ctx.reply("Welcome, " + ctx.message.from.last_name + " " + ctx.message.from.first_name));
 app.command('test', (ctx) => ctx.reply('Test'));
 
-app.use((ctx) => ctx.reply(ctx.message.text + " сказал " + ctx.message.from.last_name + " " + ctx.message.from.first_name));
+// app.use((ctx) => ctx.reply(ctx.message.text + " сказал " + ctx.message.from.last_name + " " + ctx.message.from.first_name));
 
 app.hears('hi', ctx => {
  return ctx.reply('Hey!');
