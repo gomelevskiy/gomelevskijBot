c6ad4b6b608caf4931412a340ea378a4f3b34823
админ <admin@iMac.local>
2019-04-24 19:43:32 +0300

    test

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ app.command('test', (ctx) => ctx.reply('Test'));
 // app.use((ctx) => ctx.reply(ctx.message.text + " сказал " + ctx.message.from.last_name + " " + ctx.message.from.first_name));
 
 app.hears('hi', ctx => {
- return ctx.reply(ctx.message.text);
+
+	let msg = '';
+	msg = 'Сказал мне ' + ctx.message.text;
+
+ return ctx.reply(msg);
 });
 
 app.launch();
