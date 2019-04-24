8dc3f7519c650a40bf4a6defa5d55f97a31986d3
админ <admin@iMac.local>
2019-04-24 13:52:15 +0300

    ffd

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-const Telegraf = require('telegraf');
 const app = new Telegraf('899741222:AAFV4NVV9B13QgG6quUuk0T1HI-jA1LYQh0');
 
 app.use((ctx, next) => {
-  const start = new Date();
+  const start = new Date()
   return next(ctx).then(() => {
-    const ms = new Date() - start;
-    console.log('Response time %sms', ms);
+    const ms = new Date() - start
+    console.log('Response time %sms', ms)
   });
 });
 
-app.on('text', (ctx) => ctx.reply('Hello'));
+app.on('text', (ctx) => ctx.reply('Hello '));
+app.command('test', (ctx) => ctx.reply('Test');
 app.launch();
-
-let port = process.env.PORT || 8000;
-server.listen(port, function() {
-    console.log("App is running on port " + port);
-});
