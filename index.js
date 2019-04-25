2a24da25fba67237f419e6e49c2f1acc4f809581
админ <admin@iMac.local>
2019-04-25 13:19:07 +0300

    test

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+// includes
 const Telegraf = require('telegraf');
+let telegramHandler = require('lambda-telegram-bot-handler');
+
+// init my telegram
 const app = new Telegraf(process.env.BOT_TOKEN);
 
 app.start((ctx) => ctx.reply("Welcome, " + ctx.message.from.last_name + " " + ctx.message.from.first_name));
@@ -12,6 +16,20 @@ app.hears('hi', ctx => {
 	msg = 'Сказал мне ' + ctx.message.text;
 
  return ctx.reply(msg);
+});
+
+var options = {
+  reply_markup: JSON.stringify({
+    inline_keyboard: [
+      [{ text: 'Кнопка 1', callback_data: '1' }],
+      [{ text: 'Кнопка 2', callback_data: 'data 2' }],
+      [{ text: 'Кнопка 3', callback_data: 'text 3' }]
+    ]
+  })
+};
+
+app.onText(/\/start_test/, function (msg, match) {
+  app.sendMessage(msg.chat.id, 'Выберите любую кнопку:', options);
 });
 
 app.launch();
