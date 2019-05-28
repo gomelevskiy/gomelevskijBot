f6c9fa79738a6b49755a827800a71ab679c5b6e8
Gomelevskiy <gomelevskij@gmail.com>
2019-05-28 11:58:18 +0300

    test

--- index.js
+++ index.js
@@ -69,27 +69,4 @@ app.catch(error => {
   console.log('telegraf error', error.response, error.parameters, error.on || error);
 });
 
-var wd = require('selenium-webdriver');
-var assert = require('assert');
-
-var SELENIUM_HOST = 'http://localhost:4444/wd/hub';
-var URL = 'http://www.yandex.ru';
-
-var client = new wd.Builder()
-   .usingServer(SELENIUM_HOST)
-   .withCapabilities({ browserName: 'chrome' })
-   .build();
-
-client.get(URL).then(function() {
-    client.findElement({ name: 'text' }).sendKeys('test');
-    client.findElement({ css: '.b-form-button__input' }).click();
-
-    client.getTitle().then(function(title) {
-        assert.ok(title.indexOf('test — Яндекс: нашлось') > -1, 'Ничего не нашлось :(');
-    });
-
-    client.quit();
-});
-
-
 app.launch();
