27f99fa4a7c596b93c6679fa2cdbd09720045f29
Gomelevskiy <gomelevskij@gmail.com>
2019-05-29 14:43:32 +0300

    test

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const http = require('request');
 const TelegrafInlineMenu = require('telegraf-inline-menu');
 const paramTrello = {
   page: "AvJmy7iN",
-  pageId: "5c5d92365e0bd961fa440ef4",
   key: "af5c385fa0bbcca4ef1d6c0692a95531",
   token: "2213c6ce8516841f60b276fe1c4431096b7b9333bcede41f05a791f98b90e5d9"
 };
@@ -14,8 +13,6 @@ const app = new Telegraf(process.env.BOT_TOKEN);
 
 app.start((ctx) => ctx.reply("Welcome, " + ctx.message.from.last_name + " " + ctx.message.from.first_name));
 app.command('test', (ctx) => ctx.reply('Test'));
-
-// app.use((ctx) => ctx.reply(ctx.message.text + " сказал " + ctx.message.from.last_name + " " + ctx.message.from.first_name));
 
 app.hears('hi', ctx => {
 
